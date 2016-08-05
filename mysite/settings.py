8c396c40a4c8de720906d25aa9882552a190bb4b
strong-ge <simba_cc@163.com>
2016-08-05 10:17:05 -0400

    setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'NAME': '/home/db.sqlite3',
+        'NAME': '/home/db.sqlite3',
     }
 }
 
